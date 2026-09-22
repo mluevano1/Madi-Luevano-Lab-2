e6a98208b4e32cdafc6418c49b6fadf99ddde353
mluevano1 <mluevano29@setoncatholic.org>
2026-09-22 17:58:42 +0000

    j

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-x = 20
+Import random
 
-If x > 21 
-    print ("Oh no")
+game_number = random randiant (1,10)
+print (game_number)
+
+guess=int(input ("Enter a number between 1 and 10"))
+
+If guess>game_number:
+    print ("lower")
+elif guess<game_number:
+    print ("higher!")
+else:
+    print ("Yay! You win")
