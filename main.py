514ea41b7742a6864388a4abc5e87a90440b56f9
mluevano1 <mluevano29@setoncatholic.org>
2026-09-22 18:18:41 +0000

    done

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import random
 
 game_number = random.randint (1,10)
+guesses = []
+
 while (True):
-
-    print (game_number)
-
     guess=int(input ("Enter a number between 1 and 10"))
-
+    guesses.append(guess)
 
     if guess>game_number:
-     print ("lower")
+        feedback = "lower"
     elif guess<game_number:
-        print ("higher!")
+        feedback = "higher!"
     else:
         print ("You win!")
+        print (f"You got it in {len(guesses)} guesses.")
+        print ("Guess history:")
+        for number, recorded_guess in enumerate(guesses, start=1):
+            if recorded_guess > game_number:
+                recorded_feedback = "lower"
+            elif recorded_guess < game_number:
+                recorded_feedback = "higher"
+            else:
+                recorded_feedback = "correct"
+            print (f"{number}. {recorded_guess} ({recorded_feedback})")
+        break
+
+    print (feedback)
